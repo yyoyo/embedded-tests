ea6b09e532a5e01bb23a1161d0193a2bdaf8d0d7
yoyo <yoyo@MacBook-Air-de-Elodie.local>
2018-12-01 12:46:15 +0100

    Initial commit

---
+++ src/components/ThemeEditor.vue
@@ -0,0 +1,24 @@
+<template>
+  <lyra-form
+    kr-form-token="DEMO-TOKEN-TO-BE-REPLACED"
+    kr-language="es-ES"
+    kr-post-url-success="paid.php"
+  >
+    <!-- payment form fields -->
+    <div class="kr-pan"></div>
+    <div class="kr-expiry"></div>
+    <div class="kr-security-code"></div>
+
+    <!-- payment form submit button -->
+    <button class="kr-payment-button"></button>
+
+    <!-- error zone -->
+    <div class="kr-form-error"></div>
+  </lyra-form>
+</template>
+
+<script>
+export default {
+  name: "ThemeEditor"
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+// import the compoment
+import LyraForm from "@lyracom/vue-embedded-form";
+
+//define component setup options
+const setup = {
+  clientDomain: "https://api.payzen.eu",
+  publicKey: "69876357:testpublickey_DEMOPUBLICKEY95me92597fd28tGD4r5",
+  theme: "classic"
+};
+
+// Load the component
+Vue.use(LyraForm, setup);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
